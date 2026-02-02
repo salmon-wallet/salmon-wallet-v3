5851a023e69e6c0a2517c6f9b8c67de7900f5a05
Luquish <84946392+Luquish@users.noreply.github.com>
2026-02-02 17:45:27 -0300

    chore(mobile): remove legacy component utilities

--- apps/mobile/components/__tests__/StyledText-test.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render, screen } from '@testing-library/react-native';
-
-import { MonoText } from '../StyledText';
-
-it('renders correctly', () => {
-  render(<MonoText>Test text</MonoText>);
-  expect(screen.getByText('Test text')).toBeTruthy();
-});
